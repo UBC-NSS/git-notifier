dabf0f5d50bdab89ca99049d76683a3868c4633c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-10-23 13:20:26 +0100

    Add simple line coloring for diffs

--- htmlify.py
+++ htmlify.py
@@ -1,2 +1,16 @@
+def _mangle_line(line):
+  content = line.lstrip()
+  leadingspaces = '&nbsp;' * (len(line)-len(content))
+
+  if content.startswith('-'):
+    return '<span style="color:#800">%s%s</span>' % (leadingspaces, content)
+  if content.startswith('+'):
+    return '<span style="color:#080">%s%s</span>' % (leadingspaces, content)
+  return '%s%s' % (leadingspaces, content)
+
+
 def htmlify(text):
-  return '<html><body><pre>%s</pre></body></html>' % text
+  lines = text.splitlines()
+  lines = [_mangle_line(l) for l in lines]
+
+  return '<html><body><tt>%s</tt></body></html>' % "<br>".join(lines)
